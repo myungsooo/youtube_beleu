918034f5d49b305bb47143e80222d4d5cf85a27c
myungsooo <audtn0508@naver.com>
2022-01-20 22:41:29 +0900

    폴더 변경

---
+++ Basic/Intermediate/Build/Mac/x86_64/UE4Editor/Inc/MyProject/MyActor.generated.h
@@ -0,0 +1,111 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MYPROJECT_MyActor_generated_h
+#error "MyActor.generated.h already included, missing '#pragma once' in MyActor.h"
+#endif
+#define MYPROJECT_MyActor_generated_h
+
+#define youtube_beleu_Source_MyProject_MyActor_h_12_SPARSE_DATA
+#define youtube_beleu_Source_MyProject_MyActor_h_12_RPC_WRAPPERS \
+	virtual void CallFromCpp_Implementation(); \
+ \
+	DECLARE_FUNCTION(execCalculateDPS); \
+	DECLARE_FUNCTION(execCallFromCpp);
+
+
+#define youtube_beleu_Source_MyProject_MyActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execCalculateDPS); \
+	DECLARE_FUNCTION(execCallFromCpp);
+
+
+#define youtube_beleu_Source_MyProject_MyActor_h_12_EVENT_PARMS
+#define youtube_beleu_Source_MyProject_MyActor_h_12_CALLBACK_WRAPPERS
+#define youtube_beleu_Source_MyProject_MyActor_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMyActor(); \
+	friend struct Z_Construct_UClass_AMyActor_Statics; \
+public: \
+	DECLARE_CLASS(AMyActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyProject"), NO_API) \
+	DECLARE_SERIALIZER(AMyActor)
+
+
+#define youtube_beleu_Source_MyProject_MyActor_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAMyActor(); \
+	friend struct Z_Construct_UClass_AMyActor_Statics; \
+public: \
+	DECLARE_CLASS(AMyActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyProject"), NO_API) \
+	DECLARE_SERIALIZER(AMyActor)
+
+
+#define youtube_beleu_Source_MyProject_MyActor_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyActor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyActor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyActor); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyActor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyActor(AMyActor&&); \
+	NO_API AMyActor(const AMyActor&); \
+public:
+
+
+#define youtube_beleu_Source_MyProject_MyActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyActor(AMyActor&&); \
+	NO_API AMyActor(const AMyActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyActor); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMyActor)
+
+
+#define youtube_beleu_Source_MyProject_MyActor_h_12_PRIVATE_PROPERTY_OFFSET
+#define youtube_beleu_Source_MyProject_MyActor_h_9_PROLOG \
+	youtube_beleu_Source_MyProject_MyActor_h_12_EVENT_PARMS
+
+
+#define youtube_beleu_Source_MyProject_MyActor_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	youtube_beleu_Source_MyProject_MyActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	youtube_beleu_Source_MyProject_MyActor_h_12_SPARSE_DATA \
+	youtube_beleu_Source_MyProject_MyActor_h_12_RPC_WRAPPERS \
+	youtube_beleu_Source_MyProject_MyActor_h_12_CALLBACK_WRAPPERS \
+	youtube_beleu_Source_MyProject_MyActor_h_12_INCLASS \
+	youtube_beleu_Source_MyProject_MyActor_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define youtube_beleu_Source_MyProject_MyActor_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	youtube_beleu_Source_MyProject_MyActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	youtube_beleu_Source_MyProject_MyActor_h_12_SPARSE_DATA \
+	youtube_beleu_Source_MyProject_MyActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	youtube_beleu_Source_MyProject_MyActor_h_12_CALLBACK_WRAPPERS \
+	youtube_beleu_Source_MyProject_MyActor_h_12_INCLASS_NO_PURE_DECLS \
+	youtube_beleu_Source_MyProject_MyActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MYPROJECT_API UClass* StaticClass<class AMyActor>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID youtube_beleu_Source_MyProject_MyActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Basic/Intermediate/Build/Mac/x86_64/UE4Editor/Inc/MyProject/MyProjectGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MYPROJECT_MyProjectGameModeBase_generated_h
+#error "MyProjectGameModeBase.generated.h already included, missing '#pragma once' in MyProjectGameModeBase.h"
+#endif
+#define MYPROJECT_MyProjectGameModeBase_generated_h
+
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_SPARSE_DATA
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_RPC_WRAPPERS
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMyProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_AMyProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AMyProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyProject"), NO_API) \
+	DECLARE_SERIALIZER(AMyProjectGameModeBase)
+
+
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAMyProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_AMyProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AMyProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyProject"), NO_API) \
+	DECLARE_SERIALIZER(AMyProjectGameModeBase)
+
+
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyProjectGameModeBase(AMyProjectGameModeBase&&); \
+	NO_API AMyProjectGameModeBase(const AMyProjectGameModeBase&); \
+public:
+
+
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyProjectGameModeBase(AMyProjectGameModeBase&&); \
+	NO_API AMyProjectGameModeBase(const AMyProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyProjectGameModeBase)
+
+
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_12_PROLOG
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_SPARSE_DATA \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_RPC_WRAPPERS \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_INCLASS \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_SPARSE_DATA \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	youtube_beleu_Source_MyProject_MyProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MYPROJECT_API UClass* StaticClass<class AMyProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID youtube_beleu_Source_MyProject_MyProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
